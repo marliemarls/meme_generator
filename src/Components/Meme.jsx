6cc2d53ef81f50011f7298398c12e6b786b84cf9
marliemarl <marlieahue@gmail.com>
2024-03-04 03:23:02 -0500

    api

--- src/Components/Meme.jsx
+++ src/Components/Meme.jsx
@@ -1,26 +1,45 @@
 import React from "react"
-import memesData from "../memesData.jsx"
+// import memesData from "../memesData.jsx"
 
 export default function Meme() {
+
+
+
+
+
+
+
+
+
+
+
+
     const [meme, setMeme] = React.useState({
         topText: "",
         bottomText: "",
         randomImage:"http://i.imgflip.com/1bij.jpg"}
     )
     
-    const [allMemeImages, setAllMemeImages] = React.useState(memesData)
+    const [allMemes, setAllMemes] = React.useState([])
+
+    React.useEffect(()=>{
+        fetch("https://api.imgflip.com/get_memes")
+        .then(res => res.json())
+        .then(data => setAllMemes(data.data.memes))
+    }, [])
+
+    console.log(allMemes)
 
     function getMemeImage(){
-        const memesArray = allMemeImages.data.memes
-        const randomNumber = Math.floor(Math.random() * memesArray.length)
-        const url = memesArray[randomNumber].url
+        const randomNumber = Math.floor(Math.random() * allMemes.length)
+        const url = allMemes[randomNumber].url
         setMeme(prevMeme => ({
             ...prevMeme,
             randomImage: url
         }))
     }
     
-    function handleChange () {
+    function handleChange (event) {
         const {name, value} = event.target
         setMeme(prevMeme => ({
             ...prevMeme,
